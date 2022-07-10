4bdcdc94f96759e336342a8db8038667eb8aa1b3
Leonardo Lemos <leonardo@sbsistemas.com.br>
2022-07-10 16:31:38 -0300

    build: make the rollup perform build the right way

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,28 @@
-import typescript from 'rollup-plugin-typescript'
+import babel from '@rollup/plugin-babel'
+import commonJS from '@rollup/plugin-commonjs'
+import nodeResolve from '@rollup/plugin-node-resolve'
+import dts from 'rollup-plugin-dts'
+import { terser } from 'rollup-plugin-terser'
+import typescript from 'rollup-plugin-typescript2'
+
+import path from 'path'
 
 export default {
-  entry: './src/index.ts',
-  dest: './build/bundle.js',
-  format: 'iife',
-  plugins: [typescript()],
+  input: ['./src/index.ts'],
+  output: [
+    { file: './build/bundle.js', format: 'cjs' },
+    {
+      file: './build/bundle.d.ts',
+      format: 'es',
+    },
+  ],
+  plugins: [
+    nodeResolve(),
+    commonJS(),
+    babel(path.resolve(__dirname, 'babel.config.js')),
+    typescript(),
+    terser(),
+    dts(),
+  ],
+  external: ['lodash.defaultsdeep', 'react', 'react-dom'],
 }
