587224e26e19509fa6ddf2e573c75683437c0a7e
Leonardo Lemos <leonardo@sbsistemas.com.br>
2022-07-13 21:13:35 -0300

    test(storybook): add lorem ipsum typography story

--- stories/Typography.stories.tsx
+++ stories/Typography.stories.tsx
@@ -19,3 +19,12 @@ export default {
 export const Default: ComponentStory<typeof Typography> = ({ children, ...props }) => (
   <Typography {...props}>{children}</Typography>
 )
+
+export const LoremIpsum: ComponentStory<typeof Typography> = ({ children, ...props }) => (
+  <Typography {...props}>{children}</Typography>
+)
+
+LoremIpsum.args = {
+  children:
+    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis qui veniam molestias veritatis sequi unde similique provident non odio ipsa error, ratione dolor quibusdam hic sunt laboriosam ipsam nostrum illo nulla earum, incidunt odit voluptate pariatur? Velit perferendis sequi aut ullam culpa! Veniam quisquam molestiae obcaecati nesciunt voluptas nobis veritatis laborum vitae quaerat, quam qui praesentium aliquid ab officia blanditiis! Atque sed labore, exercitationem facilis voluptatibus et, est debitis architecto perferendis sit id rerum accusantium. Consequuntur quasi magni hic laboriosam rem illum, similique recusandae nulla, id atque corporis placeat eligendi aspernatur eaque quis minus ab dolorum praesentium, fugit eius debitis?',
+}
